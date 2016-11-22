368a608b9831e39b6402d93e8f1de95bde9cc484
Paul Esson <paul.esson@blueflag.com.au>
2016-11-22 14:59:11 +1100

    Add a random float, for randomness
    Shows using the context for loading data, and also returning promises

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ import {
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import {AggregationType} from 'graphql-aggregate'
+import OpticsAgent from 'optics-agent';
 
 var data = require('./data.json');
 
@@ -72,6 +73,47 @@ var PersonType = new GraphQLObjectType({
                 },
                 favoriteFruit: {
                     type: GraphQLString
+                },
+                random: {
+                    type: GraphQLFloat,
+                    resolve: (obj, args, {random}) => {
+                        return Promise.resolve(Math.random())
+                    }
+                }
+            })
+        });
+
+var PersonType1 = new GraphQLObjectType({ 
+            name: 'PersonNoInt',
+            fields: () => ({
+                _id : {
+                    type: GraphQLID
+                },
+                guid : {
+                    type: GraphQLString
+                },
+                firstName: {
+                    type: GraphQLString,
+                    resolve: (obj) => obj.name.first
+                },
+                lastName: {
+                    type: GraphQLString,
+                    resolve: (obj) => obj.name.last 
+                }, 
+                eyeColor : {
+                    type: GraphQLString
+                },
+                company: {
+                    type: GraphQLString
+                }, 
+                address: {
+                    type: GraphQLString
+                },
+                greeting: {
+                    type: GraphQLString
+                },
+                favoriteFruit: {
+                    type: GraphQLString
                 }
             })
         });
@@ -83,16 +125,30 @@ var schema = new GraphQLSchema({
             people: {
                 type: AggregationType(PersonType),
                 resolve: (root, args, {dataloaders})=> data
+            },
+            peopleNoInt: {
+                type: AggregationType(PersonType1),
+                resolve: (root, args, {dataloaders})=> data
             }
         }
     })
 });
 
 var app = express();
-app.use('/', graphqlHTTP({
-    schema: schema,
-    pretty: true,
-    graphiql: true
-}));
+OpticsAgent.instrumentSchema(schema);
+app.use('/', OpticsAgent.middleware());
+app.use('/', (request, response, next) => {
+    const opticsContext = OpticsAgent.context(request);
+    return graphqlHTTP({
+        schema: schema,
+        pretty: true,
+        graphiql: true,
+        context: {
+            opticsContext: opticsContext,
+            random: Math.random
+        }
+    })(request, response, next);
+});
+
 app.listen(3121);
 console.log('Started on http://localhost:3121/');
